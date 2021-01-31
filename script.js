5f79d982eeafd8ceccbda2fc0d317b6f68c125d7
BHH <brucehenryhayes@yahoo.com>
2021-01-30 19:17:11 -0500

    Allinged buttons to keyboard image. Assigned buttons to alert correct note names. Centered and resized text.

--- script.js
+++ script.js
@@ -1,5 +1,74 @@
+document.querySelector("#one").addEventListener("click", () => {
+    alert("C")
+}); 
 document.querySelector("#two").addEventListener("click", () => {
-    console.log("C#")
+    alert("D")
+}); 
+document.querySelector("#three").addEventListener("click", () => {
+    alert("E")
+}); 
+document.querySelector("#four").addEventListener("click", () => {
+    alert("F")
+}); 
+document.querySelector("#five").addEventListener("click", () => {
+    alert("G")
+}); 
+document.querySelector("#six").addEventListener("click", () => {
+    alert("A")
+}); 
+document.querySelector("#seven").addEventListener("click", () => {
+    alert("B")
+}); 
+document.querySelector("#eight").addEventListener("click", () => {
+    alert("C")
+}); 
+document.querySelector("#nine").addEventListener("click", () => {
+    alert("D")
+}); 
+document.querySelector("#ten").addEventListener("click", () => {
+    alert("E")
+}); 
+document.querySelector("#eleven").addEventListener("click", () => {
+    alert("F")
+}); 
+document.querySelector("#twelve").addEventListener("click", () => {
+    alert("G")
+}); 
+document.querySelector("#thirteen").addEventListener("click", () => {
+    alert("A")
+}); 
+document.querySelector("#fourteen").addEventListener("click", () => {
+    alert("B")
+}); 
+document.querySelector("#fifteen").addEventListener("click", () => {
+    alert("C#/Db")
+}); 
+document.querySelector("#sixteen").addEventListener("click", () => {
+    alert("D#/Eb")
+}); 
+document.querySelector("#seventeen").addEventListener("click", () => {
+    alert("F#/Gb")
+}); 
+document.querySelector("#eighteen").addEventListener("click", () => {
+    alert("G#/Ab")
+}); 
+document.querySelector("#nineteen").addEventListener("click", () => {
+    alert("A#/Bb")
+}); 
+document.querySelector("#twenty").addEventListener("click", () => {
+    alert("C#/Db")
+}); 
+document.querySelector("#twentyOne").addEventListener("click", () => {
+    alert("D#/Eb")
+}); 
+document.querySelector("#twentyTwo").addEventListener("click", () => {
+    alert("F#/Gb")
+}); 
+document.querySelector("#twentyThree").addEventListener("click", () => {
+    alert("G#/Ab")
+}); 
+document.querySelector("#twentyFour").addEventListener("click", () => {
+    alert("A#/Bb")
 });
 
 
