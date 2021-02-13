69476703bb3148b9269fbbf4087bcf41fd2530b5
BHH <brucehenryhayes@yahoo.com>
2021-02-13 12:57:53 -0500

    Project commit 01

--- script.js
+++ script.js
@@ -1,74 +1,50 @@
-document.querySelector("#one").addEventListener("click", () => {
-    alert("C")
-}); 
-document.querySelector("#two").addEventListener("click", () => {
-    alert("D")
-}); 
-document.querySelector("#three").addEventListener("click", () => {
-    alert("E")
-}); 
-document.querySelector("#four").addEventListener("click", () => {
-    alert("F")
-}); 
-document.querySelector("#five").addEventListener("click", () => {
-    alert("G")
-}); 
-document.querySelector("#six").addEventListener("click", () => {
-    alert("A")
-}); 
-document.querySelector("#seven").addEventListener("click", () => {
-    alert("B")
-}); 
-document.querySelector("#eight").addEventListener("click", () => {
-    alert("C")
-}); 
-document.querySelector("#nine").addEventListener("click", () => {
-    alert("D")
-}); 
-document.querySelector("#ten").addEventListener("click", () => {
-    alert("E")
-}); 
-document.querySelector("#eleven").addEventListener("click", () => {
-    alert("F")
-}); 
-document.querySelector("#twelve").addEventListener("click", () => {
-    alert("G")
-}); 
-document.querySelector("#thirteen").addEventListener("click", () => {
-    alert("A")
-}); 
-document.querySelector("#fourteen").addEventListener("click", () => {
-    alert("B")
-}); 
-document.querySelector("#fifteen").addEventListener("click", () => {
-    alert("C#/Db")
-}); 
-document.querySelector("#sixteen").addEventListener("click", () => {
-    alert("D#/Eb")
-}); 
-document.querySelector("#seventeen").addEventListener("click", () => {
-    alert("F#/Gb")
-}); 
-document.querySelector("#eighteen").addEventListener("click", () => {
-    alert("G#/Ab")
-}); 
-document.querySelector("#nineteen").addEventListener("click", () => {
-    alert("A#/Bb")
-}); 
-document.querySelector("#twenty").addEventListener("click", () => {
-    alert("C#/Db")
-}); 
-document.querySelector("#twentyOne").addEventListener("click", () => {
-    alert("D#/Eb")
-}); 
-document.querySelector("#twentyTwo").addEventListener("click", () => {
-    alert("F#/Gb")
-}); 
-document.querySelector("#twentyThree").addEventListener("click", () => {
-    alert("G#/Ab")
-}); 
-document.querySelector("#twentyFour").addEventListener("click", () => {
-    alert("A#/Bb")
-});
+let start = true
+let currentIndex = 0
+const keys = document.querySelectorAll(".pianoKeys");
+const sharps = document.querySelectorAll(".sharpScales input");
+const flats = document.querySelectorAll(".flatScales input");
+
+function startSequence() {
+    sharps.forEach((sharp) => sharp.disabled = true);
+    flats.forEach((flat) => flat.disabled = true);
+    sharps[currentIndex].disabled = false;
+    sharps[currentIndex].focus();
+
+    // if (start) {
+    //     start = false;
+    //     var inputs = document.getElementsByTagName("INPUT");
+    //     for (var i = 0; i < inputs.length; i++) {
+    //         if (inputs[i].type === 'text') {
+    //             inputs[i].disabled = true;
+    //         }
+    //     }
+    //     document.querySelector("#ssd1").disabled = false;
+    //     document.querySelector("#ssd1").focus();
+    // }
+}
+startSequence();
+
+// document.querySelector("#one").addEventListener("click", () => {
+//     // alert("C")
+//     var inputs = document.getElementsByTagName("INPUT");
+//     for (var i = 0; i < inputs.length; i++) {
+//         if (inputs[i].disabled === false) {
+//             inputs[i].value = "C"
+//             inputs[i].disabled = true
+//             inputs[i].nextSibling.disabled = false;
+//         }
+//     }
+// });
+
+
+keys.forEach( (key) => key.addEventListener("click", (e) => {
+    // alert(e.target.dataset.pitch + " " + e.target.dataset.alt)
+    sharps[currentIndex].value = e.target.dataset.pitch;
+    currentIndex++;
+    sharps[currentIndex].disabled = false;
+    sharps[currentIndex].focus();
+}))
+
+
 
 
